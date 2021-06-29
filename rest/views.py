a7ac9cedc7763e18cb97d76a35e99fc68afd4f71
Ashutosh pradhan <ashutoshpradhan164@gmail.com>
2021-06-29 11:58:13 +0530

    csrf_exempt added we cant post data without it

--- rest/views.py
+++ rest/views.py
@@ -3,8 +3,10 @@ from django.http import HttpResponse, JsonResponse
 from .serializers import ArticleSerializer
 from rest_framework.parsers import JSONParser
 from .models import Article
+from django.views.decorators.csrf import csrf_exempt
 
 
+@csrf_exempt
 def article_list(request):
     if request.method == 'GET':
         article = Article.objects.all()
